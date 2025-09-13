846ea1f7ae99e24a29997e660648b02806073227
Codist <140372187+rkecodist@users.noreply.github.com>
2025-09-13 08:52:15 +0530

    Update vite.config.ts

--- Synq/vite.config.ts
+++ Synq/vite.config.ts
@@ -1,7 +1,19 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      // This is the line that fixes the issue for Vite.
+      // It tells Vite that whenever it sees an import starting with '@',
+      // it should look inside the './src' directory.
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
